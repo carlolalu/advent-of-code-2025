c7df5d884662297f91ee126c65e7c537d6347e1b
carlolalu <83809021+carlolalu@users.noreply.github.com>
2025-12-07 18:16:46 +0100

    Complete but better

--- dec-1/my-first-julia-code.jl
+++ dec-1/my-first-julia-code.jl
@@ -1,39 +1,48 @@
 using Base;
 
-function get_forward_steps(line::AbstractString)::UInt64
-    first_char = line[1]
-
-    steps = line[2:end]
-    steps = parse(UInt64, steps)
+function get_steps(instruction::AbstractString)::Int64
+    first_char = instruction[1]
+    steps = parse(UInt64, instruction[2:end])
 
     if first_char == 'L'
-        forward_steps = 100 - steps%100
-        return forward_steps
+        return -Int64(steps)
     elseif first_char == 'R'
         return steps
     else
         throw(DomainError(first_char, "The first command must be either `L` or `R`"))
     end
-end;
+end
+
 
 input_file = Base.open("input.txt");
 
-
 let actual_position = 50, counter_of_zeros = 0
-    for line in eachline(input_file)
-        println(line)
+    for instruction in eachline(input_file)
 
-        forward_steps = get_forward_steps(line)
-        actual_position = (actual_position + forward_steps)%100
+        println("Receiveid instruction: $instruction")
 
-        println("The actual position is: $actual_position")
+        steps = get_steps(instruction)
+        intermediate_value = actual_position + steps
 
+        println("Non-processed position after the instruction: $intermediate_value")
+
+        intermediate_value = intermediate_value%100
+        if intermediate_value < 0
+
+            println("INTERMEDIATE VALUE IS NEGATIVE: $intermediate_value")
+
+            intermediate_value = 100+intermediate_value
+        end
+
+        actual_position = intermediate_value
+
+        println("Actual position: $actual_position")
+        
         if actual_position == 0
             counter_of_zeros += 1
-            println("The counter of zeros increased to the number $counter_of_zeros")
+            println("We have one more zero, now we are at: $counter_of_zeros")
         end
     end
-    println("The final position is $actual_position and the total number of zeros is $counter_of_zeros")
-end 
 
-
+    println("Final position: $actual_position, final counter_of_zeros: $counter_of_zeros")
+end
