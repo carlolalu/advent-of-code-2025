b55724495876fdca86a36551a7238d12404ed370
carlolalu <83809021+carlolalu@users.noreply.github.com>
2025-12-18 01:34:38 +0100

    tmp - still searching

--- dec-2/exercise.jl
+++ dec-2/exercise.jl
@@ -15,48 +15,43 @@ end
 
 intervals = [get_interval_tuple(raw_interval) for raw_interval in raw_intervals];
 
+# We ignore the possibility of having overflow here
+function get_mirror_num(number::Int64)::Int64
+    len10 = Int64(Math.floor(Math.log10(number) + 1));
+    return number * (10^len10 + 1)
+end
+
 
 # Given a number abcwxyz, crafts a mirror number abcwabcw (which might not be after that)
-function get_first_mirror_number(lower_bound::Int64)#::Int64
+function get_first_base_for_mirror_number(lower_bound::Int64)#::Int64
     println("lower_bound: $lower_bound")
 
     # Operate with the length of the lower bound in decimal notation
     len10 = Int64(Math.floor(Math.log10(lower_bound) + 1));
     len10_of_part_to_discard = div(len10,2);
 
-    println("len10: $len10, len10_of_part_to_discard: $len10_of_part_to_discard")
-
     first_half_of_lower_bound = div(lower_bound, 10^len10_of_part_to_discard);
+    second_half_of_lower_bound = lower_bound - first_half_of_lower_bound * 10^len10_of_part_to_discard;
 
-    println("first_half_of_lower_bound: $first_half_of_lower_bound")
-
-
-    if len10 % len10_of_part_to_discard == 0
-        second_half_of_lower_bound = lower_bound - first_half_of_mirror_number * 10^len10_of_part_to_discard;
-
-        println("second_half_of_lower_bound: $second_half_of_lower_bound")
-
-        len10_2nd_half = Int64(Math.floor(Math.log10(second_half_of_lower_bound) + 1));
-        mirror_number = second_half_of_lower_bound * (10^len10_2nd_half + 1);
-
-        return mirror_number
-    else
-        len10_1st_half = Int64(Math.floor(Math.log10(first_half_of_lower_bound) + 1));
-        mirror_number = first_half_of_mirror_number * (10^len10_1st_half + 1);
-
-        return mirror_number
-    end
+    return max(first_half_of_lower_bound, second_half_of_lower_bound)
 end
 
-println(get_first_mirror_number(1))
-println(get_first_mirror_number(13))
-println(get_first_mirror_number(17))
-println(get_first_mirror_number(11))
-println(get_first_mirror_number(451))
-println(get_first_mirror_number(1233))
-println(get_first_mirror_number(2233))
-println(get_first_mirror_number(4390))
-println(get_first_mirror_number(90431))
-println(get_first_mirror_number(43190))
-
 # Given a number which in decimal notation is xyz, crafts the number that in decimal notation looks like xyzxyz
+
+let sum_of_dumb_ids = 0
+    for interval in intervals
+        println("Searching in interval $interval")
+
+        lower_bound = interval[1]
+        upper_bound = interval[2]
+
+        let base_of_mirror_number = get_first_base_for_mirror_number(lower_bound), mirror_num = get_mirror_num(base_of_mirror_number)
+            while mirror_num <= upper_bound
+                sum_of_dumb_ids += mirror_num
+                println("New mirror number found in the interval: $mirror_num. The sum is now $sum_of_dumb_ids")
+                mirror_num += get_mirror_num(base_of_mirror_number + 1)
+            end
+        end
+    end
+    println(sum_of_dumb_ids)
+end
